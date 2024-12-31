dc343ef39e9b5743e18e0da2c7d3b053fa8e09d3
md-Yusha <yushaoffline@gmail.com>
2024-12-31 14:02:38 +0530

    more

--- school_website/fee_manager/urls.py
+++ school_website/fee_manager/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('bill-input/', views.bill_input, name='bill_input'),
+    path('generate-bill/', views.generate_bill, name='generate_bill'),
+    path('download-bill/', views.download_bill, name='download_bill'),
 ]
